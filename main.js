061c298dd7fde0932bc78719070ff6473103505a
Steve Boyer <stephen.a.boyer@gmail.com>
2021-07-29 13:58:19 -0500

    user receives point per bar of pipes shown

--- main.js
+++ main.js
@@ -17,6 +17,9 @@ var mainState = {
     spaceKey.onDown.add(this.jump, this);    
     this.pipes = game.add.group();  
     this.timer = game.time.events.loop(1500, this.addRowOfPipes, this); 
+    this.score = 0;
+    this.labelScore = game.add.text(20, 20, "0", 
+      { font: "30px Arial", fill: "#ffffff" });   
   },
 
 
@@ -46,6 +49,8 @@ var mainState = {
     for (var i = 0; i < 8; i++)
       if (i != hole && i != hole + 1) 
         this.addOnePipe(400, i * 60 + 10);   
+    this.score += 1;
+    this.labelScore.text = this.score;  
   },
 
 

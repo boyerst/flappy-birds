2c04dee594387807403743982d7381010ca91fad
Steve Boyer <stephen.a.boyer@gmail.com>
2021-07-30 13:15:06 -0500

    style(new birdz)

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ var mainState = {
 
   preload: function() { 
     // game.load.image('bird', 'assets/bird.png'); 
-    game.load.image('bird', 'assets/favicon.ico')
+    // game.load.image('bird', 'assets/favicon.ico')
+    game.load.image('bird', 'assets/birdz4.png')
     game.load.image('pipe', 'assets/pipe.png');
     game.load.audio('jump', 'assets/jump.wav'); 
   },
